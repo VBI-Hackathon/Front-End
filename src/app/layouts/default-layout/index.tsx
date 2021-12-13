63a728f17bace42315245f06eb8534a8985a18e1
Dong Nguyen <dongnguyenvie@gmail.com>
2021-12-13 20:30:40 +0700

    added font

--- src/app/layouts/default-layout/index.tsx
+++ src/app/layouts/default-layout/index.tsx
@@ -2,10 +2,11 @@ import React, { useEffect } from 'react';
 import { Box, Container } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import styled from '@emotion/styled';
-import { BottomAppBar } from 'app/components/BottomAppBar';
+import { BottomAppBar } from 'app/components/bottom-app-bar';
 import { useSettingSlice } from 'app/stores/setting-store';
 import { selectBottomNav } from 'app/stores/setting-store/selectors';
 import { useSelector } from 'react-redux';
+import { TopAppBar } from 'app/components/top-app-bar';
 
 export function DefaultLayout() {
   const { actions } = useSettingSlice();
@@ -14,6 +15,7 @@ export function DefaultLayout() {
   return (
     <Layout>
       <ContainerStyled maxWidth="sm">
+        <TopAppBar/>
         <Box sx={{ bgcolor: 'white', minHeight: '100vh' }}>
           <Outlet />
         </Box>
